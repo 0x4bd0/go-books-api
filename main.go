5a21a95b976e35321a598221d1026c052bcbfb83
abdelkader MOUDJAR <abdoumjr@gmail.com>
2021-04-03 10:53:54 +0100

    sync

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
 )
 
 type Book struct {
@@ -14,25 +15,47 @@ type Book struct {
 }
 
 type bookHandler struct {
+	sync.Mutex
 	store map[string]Book;
+}
+
+
+func (b *bookHandler) books(writer http.ResponseWriter, request *http.Request){
+	switch request.Method {
+	case "GET" :
+			b.get(writer, request)
+			return
+	case "POST" :
+			b.get(writer, request)
+			return
+	default :
+	        writer.WriteHeader(http.StatusMethodNotAllowed)
+			writer.Write([]byte("Action not alowed"))
+			return
+	}
+}
+
+func (b *bookHandler) post(writer http.ResponseWriter, request *http.Request){
+
 }
 
 func (b *bookHandler) get(writer http.ResponseWriter, request *http.Request){
 
 	books := make([]Book, len(b.store))
 
-
+	b.Lock()
 	i := 0
 	for _, book := range b.store {
 		books[i] = book
 		i++
 	}
+	b.Unlock()
 
 	jsonBytes, err  := json.Marshal(books)
 	
 	if err != nil {
 		writer.WriteHeader(http.StatusInternalServerError)
-	     writer.Write([]byte (err.Error()))
+	    writer.Write([]byte (err.Error()))
 	}
 
 	writer.Header().Add("content-type", "application/json")
@@ -57,7 +80,7 @@ func newBookHandler() *bookHandler {
 func main () {
 	bookHandler := newBookHandler()
 
-	http.HandleFunc("/books",bookHandler.get)
+	http.HandleFunc("/books",bookHandler.books)
 
 	err := http.ListenAndServe(":1234",nil)
 
